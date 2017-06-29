6584b085986d776edfe5428e7761d5db79ccb164
xuwentao23 <903713858@qq.com>
2017-06-29 18:48:11 +0800

    新

---
+++ files/new_page_1/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,[j,k],l,_(m,n,o,p,q,r,s,_(),t,_(u,v,w,x,y,_(z,A,B,C),D,null,E,x,F,x,G,H,I,null,J,K,L,M,N,O,P,K),Q,_(),R,_(),S,_(T,[])),U,_(),V,_());}; 
+var b="url",c="new_page_1.html",d="generationDate",e=new Date(1498733265541.31),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="variables",j="n",k="playsate",l="page",m="packageId",n="8a2a3a3bf5e94ce7b9c3d1e4b51e268a",o="type",p="Axure:Page",q="name",r="New Page 1",s="notes",t="style",u="baseStyle",v="627587b6038d43cca051c114ac41ad32",w="pageAlignment",x="near",y="fill",z="fillType",A="solid",B="color",C=0xFFFFFFFF,D="image",E="imageHorizontalAlignment",F="imageVerticalAlignment",G="imageRepeat",H="auto",I="favicon",J="sketchFactor",K="0",L="colorStyle",M="appliedColor",N="fontName",O="Applied Font",P="borderWidth",Q="adaptiveStyles",R="interactionMap",S="diagram",T="objects",U="masters",V="objectPaths";
+return _creator();
+})());
